79c413ea97221448aa357717888c0e37a6a95862
Aulia Iskandar Lubis <auliaiskandarlubis31@gmail.com>
2026-01-05 21:49:14 +0700

    Push Pertama JOSSS

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [tailwind(), react()]
+});
